c7ec6368005e2645f433b963ed018bf1cc006ff4
ABINSBABUK <65391422+ABINSBABUK@users.noreply.github.com>
2020-05-16 05:38:16 +0530

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,11 @@
+arry = [] #array to hold the values
+for x in range (1,1001):  # giving the range for calculation
+  if ( x % 4 == 0 ) or ( x % 5 == 0 ) : # checking the values which are divisile by  4 or 5
+   i = 0
+   arry.insert(i,x) # inserting the diviible values to the array 
+   i = i + 1
+sum = 0
+for j in range(0, len(arry)):  #looping to add the elements inside the array  
+   sum = sum + arry[j]
+print(arry)
+print("Sum : " + str(sum))
