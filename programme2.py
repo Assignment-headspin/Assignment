a325351bbe12825f1cb3a05eec6034630b65c370
ABINSBABUK <65391422+ABINSBABUK@users.noreply.github.com>
2020-05-16 06:04:26 +0530

    Create programme2.py

---
+++ programme2.py
@@ -0,0 +1,6 @@
+ch = input("Enter the character : ") # input the character
+
+if((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')): #checking the character
+    print("Is an Alphabet")
+else:
+    print("Not an Alphabet")
