8ac2d58aba745d14d2e019217345ec6b4dde5315
ABINSBABUK <65391422+ABINSBABUK@users.noreply.github.com>
2020-05-28 11:13:39 +0530

    Update programme1.py

--- programme1.py
+++ programme1.py
@@ -1,5 +1,5 @@
 arry = [] #array to hold the values
-for x in range (1,1001):  # giving the range for calculation
+for x in range (1,1000):  # giving the range for calculation
   if ( x % 4 == 0 ) or ( x % 5 == 0 ) : # checking the values which are divisile by  4 or 5
    i = 0
    arry.insert(i,x) # inserting the diviible values to the array 
